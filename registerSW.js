1ac274da6b24ae98715c89fcc1a5cbb81ec9e663
Daniel <dbola122z@fpdrioja.com>
2025-07-15 20:48:45 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/keepmyhair/sw.js', { scope: '/keepmyhair/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
